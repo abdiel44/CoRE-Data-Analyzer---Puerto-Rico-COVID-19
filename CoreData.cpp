b75c0f795c2a3947362c260c62fd8dfda54b7df2
Steven Matias <stevenmatias2544@gmail.com>
2020-05-09 15:20:37 -0400

    The function that displays the menu and the user's selection was created.

--- CoreData.cpp
+++ CoreData.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+char displayMenu();
+int choice;
 
 int main()
 {
-    cout<<"Hello World";
+ displayMenu();
+}
+
+char displayMenu()
+{
+    cout<<"------------------------------------------"<<endl;
+    cout<<"|  CoRE COVID-19 Dashboard and Analyzer  |"<<endl;
+    cout<<"------------------------------------------"<<endl;
+    cout<<"|1." << setw(20) << "Display Data Frame" << setw(19) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|2." << setw(16) << "Display a town" << setw(23) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|3." << setw(12) << "Add a town" << setw(27) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|4." << setw(25) << "Edit a town observation" << setw(14) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|5." << setw(15) << "Delete a town" << setw(24) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|6." << setw(22) << "Find towns by region" << setw(17) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|7." << setw(31) << "Find observation by town name" << setw(8) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|8." << setw(27) << "Save observations to file" << setw(12) << "|" << endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|9." << setw(29) << "Load observations from file" << setw(10) << "|" <<endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|10." << setw(17) << "Create Bar chart" << setw(21) << "|" <<endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|11." << setw(27) << "Show Puerto Rico Dashboard" << setw(11) << "|" <<endl;
+    cout<<"|----------------------------------------|"<<endl;
+    cout<<"|12." << setw(17) << "Exit application" << setw(21) << "|" <<endl;
+    cout<<"------------------------------------------"<<endl;
+    cout<<"     Enter option number: ";
+    cin>>choice;
+    cout<<"------------------------------------------"<<endl;
+
+    return choice;
 }
